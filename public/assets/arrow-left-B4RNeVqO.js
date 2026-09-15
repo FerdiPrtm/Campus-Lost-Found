3a53517f312598a57ea73bf59e90bd1f6154e4b5
Ferdi Pratama <ferdipratamaaa27@gmail.com>
2026-09-15 16:05:34 +0700

    build: icon chunks update (arrow, circle-check)

--- public/assets/arrow-left-B4RNeVqO.js
+++ public/assets/arrow-left-B4RNeVqO.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DcrnSpYV.js";/**
+import{c as e}from"./index-X04dJ4HI.js";/**
  * @license lucide-vue-next v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- public/assets/circle-check-CzkAz5zs.js
+++ public/assets/circle-check-CzkAz5zs.js
@@ -1,4 +1,4 @@
-import{c}from"./index-DcrnSpYV.js";/**
+import{c}from"./index-X04dJ4HI.js";/**
  * @license lucide-vue-next v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
