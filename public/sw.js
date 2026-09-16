a7e00afc194c7290d12cadd32a0db6300b799e6b
Ferdi Pratama <ferdipratamaaa27@gmail.com>
2026-09-16 15:20:10 +0700

    build: sw.js -> public

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE = "campus-lf-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) =>
+      c.addAll(["/", "/manifest.json", "/icon-192.png", "/icon-512.png"])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      const fetched = fetch(e.request).then((res) => {
+        if (res.ok && e.request.url.startsWith(self.location.origin)) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      });
+      return hit || fetched;
+    })
+  );
+});
